874965146681263d3ceb0930627811287e96dd34
Philip Corr <corrphilip@gmail.com>
2016-12-29 10:22:10 +0000

    added routes and new components

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,6 +1,7 @@
 import Terrain from 'react-icons/lib/md/terrain'
 import SnowFlake from 'react-icons/lib/ti/weather-snow'
 import Calendar from 'react-icons/lib/fa/calendar'
+import { PropTypes } from 'react'
 
 const percentToDecimal = (decimal) => {
 	return((decimal*100) + '%')
@@ -37,3 +38,17 @@ export const SkiDayCount = ({total, powder, backcountry, goal}) => (
 		</div>
 	</div>
 )
+
+SkiDayCount.defaultProps = {
+	total: 50,
+	powder: 10,
+	backcountry: 15,
+	goal: 75
+}
+
+SkiDayCount.propTypes = {
+	total: PropTypes.number,
+	powder: PropTypes.number,
+	backcountry: PropTypes.number,
+	goal: PropTypes.number
+}
--- src/index.js
+++ src/index.js
@@ -1,34 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayList } from './components/SkiDayList'
+import { App } from './components/App'
+import { Whoops404 } from './components/Whoops404'
+import { Router, Route, hashHistory } from 'react-router'
 
 //Avoid problem with react not being defined
 window.React = React
 
 //render all components
 render(
-	// properties being passed in as JSX
-    <SkiDayList days={
-    	[
-    		{
-    			resort: "Squaw Valley",
-    			date: new Date("1/2/2016"),
-    			powder: true,
-    			backcountry: false
-    		},
-    		{
-    			resort: "Kirkwood",
-    			date: new Date("3/28/2016"),
-    			powder: false,
-    			backcountry: false
-    		},
-    		{
-    			resort: "Mt. Tallac",
-    			date: new Date("4/2/2016"),
-    			powder: false,
-    			backcountry: true
-    		}
-    	]
-    }/>,
+	// hashHistory listens to address bar for changes
+    <Router history={hashHistory}>
+    	<Route path="/" component={App}/>
+    	<Route path="list-days" component={App}>
+    		<Route path=":filter" component={App}/>
+    	</Route>
+    	<Route path="add-day" component={App}/>
+    	<Route path="*" component={Whoops404}/>
+    </Router>,
     document.getElementById('react-container')
 )
